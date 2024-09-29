6ade43b6cb9d5c1b65bc304c8325d8f398f0cdbe
Raihan H <raihanhasan427@gmail.com>
2024-09-29 00:08:22 -0400

    html buttons and ui complete but functionality in works

---
+++ script.js
@@ -0,0 +1,19 @@
+const fileInput = document.querySelector(".file-input"),
+filterOptions = document.querySelectorAll(".filter button");
+previewImg = document.querySelector(".preview-img img");
+chooseImgBtn = document.querySelector(".choose-img");
+
+const loadImage = () => {
+    let file = fileInput.files[0];
+    if (!file) return;
+    previewImg.src = URL.createObjectURL(file);
+}
+
+filterOptions.forEach(option => {
+    option.addEventListener("click", () => {
+        document.querySelector(".filter .active").classList.remove( "active" );
+        option.classList.add("active");
+    });
+});
+fileInput.addEventListener("change",loadImage);
+chooseImgBtn.addEventListener("click", () => fileInput.click());
